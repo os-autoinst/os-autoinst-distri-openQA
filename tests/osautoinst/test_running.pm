eb17c1beb2fa0d2062b6daabf2e6178443f06e6c
Oliver Kurz <okurz@suse.de>
2016-12-06 08:18:21 +0100

    Add post fail diagnosis for not running job

--- tests/osautoinst/test_running.pm
+++ tests/osautoinst/test_running.pm
@@ -8,8 +8,21 @@ sub run {
     type_string "clear\n";
 }
 
+sub get_log {
+    my ($cmd, $name) = @_;
+    my $ret = script_run "$cmd | tee $name";
+    upload_logs $name if $ret;
+}
+
 sub post_fail_hook {
-    assert_script_run 'openqa-client jobs';
+    script_run 'openqa-client jobs';
+    save_screenshot;
+    get_log 'journalctl --pager-end --no-tail --no-pager -u apache2 -u openqa-scheduler -u openqa-websockets -u openqa-webui -u openqa-worker@1' => 'openqa_services.log';
+    get_log 'cat /var/lib/openqa/pool/1/autoinst-log.txt /var/lib/openqa/testresults/*/*/autoinst-log.txt' => 'autoinst-log.txt';
+    script_run 'lsmod | grep kvm';
+    save_screenshot;
+    get_log 'grep --color -z -E "(vmx|svm)" /proc/cpuinfo' => 'cpuinfo';
+    assert_script_run 'grep --color -z -E "(vmx|svm)" /proc/cpuinfo', fail_message => 'Machine does not support nested virtualization, please enable in worker host';
 }
 
 sub test_flags {
