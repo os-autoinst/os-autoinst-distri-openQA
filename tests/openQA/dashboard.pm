e0bb29d68710d538fa147a67a0ecb6b49bbb47cb
Joachim Rauch <jora@pinky.arch.suse.de>
2017-07-10 10:12:53 +0200

    Revert dashboard.pm to original state
    Reverted dashboard.pm back to the original state, since the
    problem #19798 does not appear anymore

--- tests/openQA/dashboard.pm
+++ tests/openQA/dashboard.pm
@@ -9,11 +9,6 @@ sub run {
     ensure_unlocked_desktop();
     x11_start_program("firefox http://localhost", 60, { valid => 1 } );
     # starting from git might take a bit longer to get and generated assets
-    # workaround for poo#19798, basically doubles the timeout
-    if ((check_screen 'openqa-dashboard', 180) == undef) {
-        record_soft_failure 'ff took to long to start';
-    }
-    #wait few minutes for ff to start and then fail the test
-    assert_screen 'openqa-dashboard', 360;
+    assert_screen 'openqa-dashboard', check_var('OPENQA_FROM_GIT',1) ? 180 : 60;
 }
 1;
