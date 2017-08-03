33f3c3a889a78ae39774b95a0adae1a6db906ad8
Joachim Rauch <jrauch@suse.com>
2017-08-03 11:12:01 +0200

    Make job querying more reliable with JSON parsing and bash variable check

--- tests/osautoinst/start_test.pm
+++ tests/osautoinst/start_test.pm
@@ -15,8 +15,12 @@ sub run {
         my $cmd = <<"EOF";
 last_tw_build=\$(openqa-client --host $openqa_url assets get | sed -n 's/^.*name.*Tumbleweed-NET-$arch-Snapshot\\([0-9]\\+\\)-Media.*\$/\\1/p' | sort -n | tail -n 1)
 echo "Last Tumbleweed build on openqa.opensuse.org: \$last_tw_build"
-job_id=\$(openqa-client --host $openqa_url jobs get version=Tumbleweed scope=relevant arch=$arch build=\$last_tw_build flavor=NET | grep -B $range 'name.*$ttest' | grep -A $range group_id | sed -n 's/^\\s*\\<id.*=> \\([0-9]\\+\\).*\$/\\1/p' | sort -n | tail -n 1)
-echo "scenario $arch-$ttest-NET: \$job_id"
+[ ! -z \$last_tw_build ]
+zypper -n in jq
+job_id=\$(openqa-client --host $openqa_url --json-output jobs get version=Tumbleweed scope=relevant arch=$arch build=\$last_tw_build flavor=NET latest=1 | jq '.jobs | .[] | select(.test == "$ttest") | .id')
+echo "Job Id: \$job_id"
+[ ! -z \$job_id  ]
+echo "Scenario: $arch-$ttest-NET: \$job_id"
 sudo -u _openqa-worker touch /var/lib/openqa/factory/iso/.test || (echo "TODO: workaround, _openqa-worker should be able to write factory/iso" && mkdir -p /var/lib/openqa/factory/iso && chmod ugo+rwX /var/lib/openqa/factory/iso)
 ls -la /var/lib/openqa/factory/iso
 echo "Prevent bsc#1027347"
