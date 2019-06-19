9e4b67d8173de94e052980fb3f75d8e7c87c17b5
Oliver Kurz <okurz@suse.de>
2019-06-19 14:22:44 +0200

    Delete obsolete openSUSE 13.2 support

--- tests/update/zypper_up.pm
+++ tests/update/zypper_up.pm
@@ -15,9 +15,6 @@ sub run {
     script_run "systemctl stop packagekit.service";
     save_screenshot;
     type_string "clear\n";
-    # there is no main openqa repo for openSUSE13.2 anymore but stable
-    type_string "zypper rr openQA\n";
-    type_string "zypper ar http://download.opensuse.org/repositories/devel:/openQA:/stable/openSUSE_13.2 openQA-stable\n";
     assert_script_run('zypper -n up --auto-agree-with-licenses', timeout => 700, fail_message => 'zypper failed to update packages');
     save_screenshot;
 }
