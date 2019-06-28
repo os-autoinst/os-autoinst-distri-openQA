ebf6d58ee78c925d901ee7f6c7ea518d4f54c978
Oliver Kurz <okurz@suse.de>
2019-06-28 14:08:27 +0200

    Use more current SLES-12SP5 for unused repos anyway

--- tests/install/openqa_webui.pm
+++ tests/install/openqa_webui.pm
@@ -22,10 +22,10 @@ zypper ar -f obs://devel:openQA/openSUSE_Leap_42.3 openQA
 zypper ar -f obs://devel:openQA:Leap:42.3/openSUSE_Leap_42.3 openQA-perl-modules
 EOF
     }
-    elsif (check_var('VERSION', 'SLES-12SP1')) {
+    elsif (check_var('VERSION', 'SLES-12SP5')) {
         $add_repo = <<'EOF';
-zypper ar -f http://download.opensuse.org/repositories/devel:/openQA/SLE_12_SP1/devel:openQA.repo
-zypper ar -f http://download.opensuse.org/repositories/devel:/openQA:/SLE-12/SLE_12_SP1/devel:openQA:SLE-12.repo
+zypper ar -f http://download.opensuse.org/repositories/devel:/openQA/SLE_12_SP5/devel:openQA.repo
+zypper ar -f http://download.opensuse.org/repositories/devel:/openQA:/SLE-12/SLE_12_SP5/devel:openQA:SLE-12.repo
 EOF
     }
     else {
