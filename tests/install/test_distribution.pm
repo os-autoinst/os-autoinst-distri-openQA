fc04b68aabb1b4f1f37a4cf57fb52843a1b62254
Tina Mueller <tina.mueller@suse.com>
2019-11-19 12:16:04 +0100

    Retry zypper installation
    Currently the installation can fail if the package is modified
    during the command.

--- tests/install/test_distribution.pm
+++ tests/install/test_distribution.pm
@@ -9,7 +9,24 @@ sub run {
     diag('initialize working copy of openSUSE tests distribution with correct user');
     assert_script_run('username=bernhard email=bernhard@susetest /usr/share/openqa/script/fetchneedles', 3600);
     save_screenshot;
-    assert_script_run('zypper --no-refresh -n in os-autoinst-distri-opensuse-deps', 600);
+
+    # 104 is the zypper exit code if a package cannot be found
+    my $cmd = <<'EOM';
+(
+    for i in {1..7}
+    do
+        zypper --non-interactive install os-autoinst-distri-opensuse-deps && exit 0
+        rc=$?
+        (( $rc == 104 )) || break
+    done
+    exit $rc
+)
+EOM
+    chomp $cmd;
+    my $ret = assert_script_run(
+        $cmd,
+        timeout => 600,
+    );
     type_string "clear\n";
     # prepare for next test
     type_string "logout\n";
