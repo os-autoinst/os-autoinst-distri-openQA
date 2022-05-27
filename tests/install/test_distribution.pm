c9c62c0e3b55d65a88a8a00a9d2abb89762f2e7a
Oliver Kurz <okurz@suse.de>
2022-05-27 12:46:46 +0200

    Accept random slow 'zypper ref' calls
    https://openqa.opensuse.org/tests/2383839#step/test_distribution/4 hit
    the timeout due to extra-slow execution of zypper ref. I have also seen
    slow zypper ref executions in many other occassions so I guess we should
    just live with that.

--- tests/install/test_distribution.pm
+++ tests/install/test_distribution.pm
@@ -9,7 +9,7 @@ sub run {
     diag('initialize working copy of openSUSE tests distribution with correct user');
     assert_script_run('username=bernhard email=bernhard@susetest /usr/share/openqa/script/fetchneedles', 3600);
     save_screenshot;
-    assert_script_run('zypper -n ref -f',                              60);
+    assert_script_run('zypper -n ref -f',                              300);
     assert_script_run('zypper -n in os-autoinst-distri-opensuse-deps', 600);
     clear_root_console;
     # prepare for next test
