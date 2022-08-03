5471a944fe21fdbae28c9fb62babeec07e531e2e
Cris Dywa <cdywan@suse.de>
2022-08-03 20:45:55 +0200

    Drop x11_start_program from susedistribution
    See: https://progress.opensuse.org/issues/108530

--- lib/susedistribution.pm
+++ lib/susedistribution.pm
@@ -5,25 +5,4 @@ use base 'distribution';
 
 use testapi qw(send_key assert_screen check_screen save_screenshot type_string mouse_hide);
 
-sub x11_start_program {
-    my ($self, $program, $timeout, $options) = @_;
-    send_key 'alt-f2';
-    mouse_hide(1);
-    assert_screen('desktop-runner', $timeout);
-    type_string $program;
-    if ($options->{terminal}) {
-        wait_screen_change { send_key 'alt-t' };
-    }
-    save_screenshot;
-    send_key 'ret';
-    # make sure desktop runner executed and closed when have had valid value
-    # exec x11_start_program( $program, $timeout, { valid => 1 } );
-    if ($options->{valid}) {
-        foreach my $i (1 .. 3) {
-            last unless check_screen 'desktop-runner-border', 2;
-            send_key 'ret';
-        }
-    }
-}
-
 1;
