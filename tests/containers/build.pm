3fa2401c252c0f100aaa813942d8b6f8be295c66
Marius Kittler <mkittler@suse.de>
2022-09-01 14:13:22 +0200

    Fix likely typo in test for building `openqa_data` container

--- tests/containers/build.pm
+++ tests/containers/build.pm
@@ -6,7 +6,7 @@ sub run {
     my ($self) = @_;
     assert_script_run("git clone https://github.com/os-autoinst/openQA.git", timeout => 300);
     assert_script_run("retry -s 30 -- docker build openQA/container/$_ -t openqa_$_", timeout => 2400) for qw(webui worker);
-    assert_script_run('retry -s 30 -- docker build openQA/container/openqa_data -t openqa_datai', timeout => 3600);
+    assert_script_run('retry -s 30 -- docker build openQA/container/openqa_data -t openqa_data', timeout => 3600);
 }
 
 1;
