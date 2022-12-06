32c8acdaa6f2ca09a7f5c423aba0cfb4e82c1d03
Marius Kittler <mkittler@suse.de>
2022-12-06 16:22:40 +0100

    Avoid openQA worker from appearing broken due to no FQDN
    See https://progress.opensuse.org/issues/121567

--- tests/install/openqa_worker.pm
+++ tests/install/openqa_worker.pm
@@ -8,7 +8,7 @@ sub run {
     assert_script_run('retry -s 30 -- sh -c "zypper -n --gpg-auto-import-keys ref && zypper --no-cd -n in openQA-worker"', 600);
     diag('Login once with fake authentication on openqa webUI to actually create preconfigured API keys for worker authentication');
     assert_script_run('curl http://localhost/login');
-    diag('adding temporary, preconfigured API keys to worker config');
+    diag('adding temporary, preconfigured API keys to client config');
     type_string('cat >> /etc/openqa/client.conf <<EOF
 [localhost]
 key = 1234567890ABCDEF
@@ -17,6 +17,7 @@ EOF
 ');
     wait_still_screen(1);
     my $worker_setup = <<'EOF';
+echo "WORKER_HOSTNAME=localhost" >> /etc/openqa/workers.ini
 systemctl start openqa-worker@1
 systemctl status --no-pager openqa-worker@1
 systemctl enable openqa-worker@1
