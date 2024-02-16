f722ae2e3f466a3c1a9b9005217b612d52c38f25
Marius Kittler <mkittler@suse.de>
2024-02-16 17:01:39 +0100

    Ensure XScreenSaver is not interfering with long-running tests

--- tests/install/openqa_webui.pm
+++ tests/install/openqa_webui.pm
@@ -70,15 +70,6 @@ sub install_from_bootstrap {
 }
 
 sub run {
-    switch_to_root_console;
-    assert_screen 'inst-console';
-    type_string "root\n";
-    assert_screen 'password-prompt';
-    type_password;
-    send_key 'ret';
-    wait_still_screen(2);
-    disable_packagekit;
-    assert_script_run('for i in {1..7}; do zypper --no-cd -n in retry && break; sleep $((i**2*20)); done');
     if (get_var('OPENQA_FROM_GIT')) {
         if (get_var('OPENQA_CONTAINERS')) {
             install_containers;
