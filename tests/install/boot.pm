a10d1e5aa2d3285472608e3d03fe2dcaa506c47d
Marius Kittler <mkittler@suse.de>
2024-02-16 17:01:39 +0100

    Prevent running into DPMS timeout

--- tests/install/boot.pm
+++ tests/install/boot.pm
@@ -1,8 +1,20 @@
 use Mojo::Base 'openQAcoretest';
+use testapi;
 use utils;
+
+sub _prevent_inactivity_timeout {
+    my $desktop_runner_hotkey = check_var('DESKTOP', 'minimalx') ? 'ctrl-alt-spc' : 'alt-f2';
+    send_key $desktop_runner_hotkey;
+    assert_screen 'desktop-runner';
+    mouse_hide 1;
+    wait_still_screen 2;
+    type_string 'xset s off -dpms';
+    send_key 'ret';
+}
 
 sub run {
     wait_for_desktop;
+    _prevent_inactivity_timeout;
 }
 
 1;
