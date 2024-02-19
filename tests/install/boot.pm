8eb741c6ab94a074bdc7302d60ed49910ffaffe2
Marius Kittler <mkittler@suse.de>
2024-02-19 11:33:08 +0100

    Use `start_gui_program` to prevent inactivity timeout

--- tests/install/boot.pm
+++ tests/install/boot.pm
@@ -2,15 +2,9 @@ use Mojo::Base 'openQAcoretest';
 use testapi;
 use utils;
 
-sub _prevent_inactivity_timeout {
-    my $desktop_runner_hotkey = check_var('DESKTOP', 'minimalx') ? 'ctrl-alt-spc' : 'alt-f2';
-    send_key $desktop_runner_hotkey;
-    assert_screen 'desktop-runner';
-    mouse_hide 1;
-    wait_still_screen 2;
-    type_string 'xset s off -dpms';
-    send_key 'ret';
-}
+use OpenQA::Wheel::Launcher 'start_gui_program';
+
+sub _prevent_inactivity_timeout { start_gui_program('xset s off -dpms', undef, valid => 1) }
 
 sub run {
     wait_for_desktop;
