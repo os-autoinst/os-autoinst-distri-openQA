4b9352d833cf67e91e21afd3f85534cfe1c888d2
Liv Dywan <liv.dywan@suse.com>
2024-05-27 13:01:20 +0200

    Adjust timeout of container build calls to retry max
    The default timeout needs to be higher than the hypothetical
    maximum time of all retry attempts.
    
    See: https://progress.opensuse.org/issues/160727

--- tests/containers/build.pm
+++ tests/containers/build.pm
@@ -2,9 +2,10 @@ use Mojo::Base 'openQAcoretest';
 use testapi;
 
 sub run {
-    assert_script_run('retry -s 30 -r 7 -e -- git clone https://github.com/os-autoinst/openQA.git', timeout => 300);
-    assert_script_run("retry -s 30 -r 7 -e -- docker build openQA/container/$_ -t openqa_$_", timeout => 3800) for qw(webui worker);
-    assert_script_run('retry -s 30 -r 7 -e -- docker build openQA/container/openqa_data -t openqa_data', timeout => 3800);
+    # The maximum of the retry is 3810 seconds
+    assert_script_run('retry -s 30 -r 7 -e -- git clone https://github.com/os-autoinst/openQA.git', timeout => 4000);
+    assert_script_run("retry -s 30 -r 7 -e -- docker build openQA/container/$_ -t openqa_$_", timeout => 4000) for qw(webui worker);
+    assert_script_run('retry -s 30 -r 7 -e -- docker build openQA/container/openqa_data -t openqa_data', timeout => 4000);
 }
 
 1;
