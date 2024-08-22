3c52a0a43374ed990c34cc444a905399f0381f41
Jan Baier <jbaier@suse.cz>
2024-08-22 12:23:12 +0200

    Add option to enforce AppArmor profiles in tests
    Introduce variable `USE_APPARMOR` to allow enforcing AppArmor profiles
    inside tests. Use the new variable inside nginx install variant of the
    test.
    
    Reference: https://progress.opensuse.org/issues/155413

---
+++ tests/install/apparmor.pm
@@ -0,0 +1,17 @@
+use Mojo::Base 'openQAcoretest', -signatures;
+use testapi;
+use utils qw(install_packages);
+
+sub run {
+    # Make sure AppArmor is installed and enabled
+    install_packages('-t pattern apparmor');
+    assert_script_run('systemctl enable --now apparmor');
+    assert_script_run('aa-enabled');
+    # Reload AppArmor to enforce newly installed profiles
+    assert_script_run('systemctl reload apparmor');
+    # Restart running services to apply loaded profiles
+    assert_script_run('systemctl try-restart openqa-*');
+    assert_script_run('aa-status --filter.profiles="usr.share.openqa.*"');
+}
+
+1;
