6874c65383161833faefb1e1af837a26c1714784
Jan Baier <jbaier@suse.cz>
2024-09-20 11:33:45 +0200

    Upload docker logs when the build fails
    Reference: https://progress.opensuse.org/issues/165992

--- tests/containers/build.pm
+++ tests/containers/build.pm
@@ -1,11 +1,17 @@
-use Mojo::Base 'openQAcoretest';
+use Mojo::Base 'openQAcoretest', -signatures;
 use testapi;
 
 sub run {
     # The maximum of the retry is 3810 seconds
     assert_script_run('retry -s 30 -r 7 -e -- git clone https://github.com/os-autoinst/openQA.git', timeout => 4000);
-    assert_script_run("retry -s 30 -r 7 -e -- docker build openQA/container/$_ -t openqa_$_", timeout => 4000) for qw(webui worker);
-    assert_script_run('retry -s 30 -r 7 -e -- docker build openQA/container/openqa_data -t openqa_data', timeout => 4000);
+    assert_script_run("retry -s 30 -r 7 -e -- bash -o pipefail -c 'docker build openQA/container/$_ -t openqa_$_ --progress=plain 2>&1 | tee docker_build.txt'", timeout => 4000) for qw(webui worker);
+    assert_script_run("retry -s 30 -r 7 -e -- bash -o pipefail -c 'docker build openQA/container/openqa_data -t openqa_data --progress=plain 2>&1 | tee docker_build.txt'", timeout => 4000);
+}
+
+sub post_fail_hook ($self) {
+    save_screenshot;
+    upload_logs 'docker_build.txt';
+    $self->SUPER::post_fail_hook;
 }
 
 1;
