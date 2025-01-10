2de2f5d1318cce34c104235d92f36126ca45d5ab
Jan Baier <jbaier@suse.cz>
2025-01-10 17:23:27 +0100

    Visit both ping tests in multimachine scenario
    Reference: https://progress.opensuse.org/issues/169249

--- tests/openQA/tests.pm
+++ tests/openQA/tests.pm
@@ -1,13 +1,18 @@
-use Mojo::Base 'openQAcoretest';
+use Mojo::Base 'openQAcoretest', -signatures;
 use testapi;
 use utils;
 
-sub run {
+sub visit_test($needle) {
     assert_and_click 'openqa-all-tests';
-    send_key_until_needlematch 'openqa-scheduled-test', 'down';
-    assert_and_click 'openqa-scheduled-test';
+    send_key_until_needlematch $needle, 'down';
+    assert_and_click $needle;
     assert_screen 'openqa-test-details';
     assert_and_click 'openqa-logo';
+}
+
+sub run {
+    visit_test 'openqa-scheduled-test';
+    visit_test 'openqa-scheduled-test-ping-client' if get_var('FULL_MM_TEST');
     assert_screen 'openqa-dashboard';
 }
 
