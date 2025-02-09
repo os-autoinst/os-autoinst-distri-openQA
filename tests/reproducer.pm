20c7e93e3f2ad1fd33d4fda2e4f6e67a8e318157
Jan Baier <jbaier@suse.cz>
2025-02-09 13:10:19 +0100

    [DO NOT MERGE] Reproducer for post_run_hook not running

--- tests/reproducer.pm
+++ tests/reproducer.pm
@@ -5,6 +5,10 @@ sub run {
     assert_script_run("/bin/false");
 }
 
+sub post_run_hook {
+    record_info 'this will not run on test failure';
+}
+
 sub post_fail_hook {
     force_soft_failure 'this should be only a softfail';
 }
