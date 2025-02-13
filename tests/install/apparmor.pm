a27522bd3dc91d971a959f50740b2addd21a7785
Jan Baier <jbaier@suse.cz>
2025-02-13 12:20:46 +0100

    Enable apparmor if disabled at boot
    Reference: https://progress.opensuse.org/issues/177081

--- tests/install/apparmor.pm
+++ tests/install/apparmor.pm
@@ -1,11 +1,23 @@
 use Mojo::Base 'openQAcoretest', -signatures;
 use testapi;
-use utils qw(install_packages);
+use utils qw(login install_packages wait_for_desktop);
+
+sub enable_apparmor {
+    assert_script_run('sed -i -e "s/^GRUB_CMDLINE_LINUX_DEFAULT=\"[^\"]\+/& security=apparmor apparmor=1/" /etc/default/grub');
+    assert_script_run('update-bootloader');
+    assert_script_run('systemctl reboot');
+    wait_for_desktop;
+    login;
+    # Fix worker after reboot
+    assert_script_run('curl http://localhost/login');
+    assert_script_run('systemctl restart openqa-worker@1');
+}
 
 sub run {
     # Make sure AppArmor is installed and enabled
     install_packages('-t pattern apparmor');
     assert_script_run('systemctl enable --now apparmor');
+    enable_apparmor if script_output('aa-enabled', proceed_on_failure => 1) =~ m/disabled at boot/;
     assert_script_run('aa-enabled');
     # Reload AppArmor to enforce newly installed profiles
     assert_script_run('systemctl reload apparmor');
