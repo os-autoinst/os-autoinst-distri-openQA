29682a2e25ab5f518754bb259c4b6dc595df00aa
Oliver Kurz <okurz@suse.de>
2025-10-24 07:30:28 +0200

    Fix wait for scheduled when details already show
    Related progress issue: https://progress.opensuse.org/issues/191112

--- tests/openQA/tests.pm
+++ tests/openQA/tests.pm
@@ -10,16 +10,12 @@ sub visit_test($needle) {
     # in case of "full openSUSE test" the test is likely still running.
     # Because the content on the /tests page can move around if rendering has
     # not yet completed we need to be more creative
-    if (get_var('FULL_OPENSUSE_TEST')) {
-        my $tries_left = TRIES;
-        do {
-            die "Failed to find openQA test details after multiple retries" if --$tries_left == 0;
-            wait_screen_change { assert_and_click $needle };
-        } until (check_screen 'openqa-test-details');
-    }
-    else {
+    wait_screen_change { assert_and_click $needle };
+    for (1 .. TRIES) {
+        die "Failed to find openQA test details after multiple retries" if $_ == TRIES;
+        assert_screen [$needle, 'openqa-test-details'];
+        last if match_has_tag 'openqa-test-details';
         click_lastmatch;
-        assert_screen 'openqa-test-details';
     }
     assert_and_click 'openqa-logo';
     assert_screen 'openqa-dashboard';
