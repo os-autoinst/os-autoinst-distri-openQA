57a3a06f8b770b9eb19823224a408b9f9fe32815
Oliver Kurz <okurz@suse.de>
2026-01-08 16:21:54 +0100

    feat: retry on temporary host errors
    As observed in https://openqa.opensuse.org/tests/5588905#step/start_test/3

--- tests/osautoinst/start_test.pm
+++ tests/osautoinst/start_test.pm
@@ -35,7 +35,7 @@ sub example_run {
     my $arch = get_var('ARCH');
     my $casedir = 'https://github.com/os-autoinst/os-autoinst-distri-example.git';
     my $needlesdir = '%%CASEDIR%%/needles';
-    assert_script_run 'wget https://raw.githubusercontent.com/os-autoinst/os-autoinst-distri-example/main/scenario-definitions.yaml';
+    assert_script_run 'wget --retry-on-host-error https://raw.githubusercontent.com/os-autoinst/os-autoinst-distri-example/main/scenario-definitions.yaml';
     assert_script_run "openqa-cli schedule --param-file SCENARIO_DEFINITIONS_YAML=scenario-definitions.yaml DISTRI=example VERSION=0 FLAVOR=DVD ARCH=$arch TEST=simple_boot _GROUP_ID=0 BUILD=test CASEDIR=$casedir NEEDLES_DIR=$needlesdir";
 
 }
