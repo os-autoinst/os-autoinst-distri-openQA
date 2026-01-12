2aaa1ff472295135083966401a195db85486efa0
Oliver Kurz <okurz@suse.de>
2026-01-12 19:41:44 +0100

    feat: use SELinux rule only where needed
    openQA scripting with "configure-web-proxy" already calls the according
    necessary semanage call so we do not need to do that anymore in general.
    Only for the "manual git checkout" setup we still need it so move it
    there.
    
    Related progress issue: https://progress.opensuse.org/issues/180029

--- tests/install/prepare.pm
+++ tests/install/prepare.pm
@@ -4,10 +4,6 @@ use utils qw(login disable_packagekit switch_to_root_console);
 
 sub run {
     login;
-
-    # SELinux: allow web proxy to connect to openQA backend
-    assert_script_run('semanage boolean -m -1 httpd_can_network_connect');
-
     disable_packagekit;
     assert_script_run('for i in {1..7}; do zypper --no-cd -n in retry && break; sleep $((i**2*20)); done');
     assert_script_run('zypper --no-cd -n rm xscreensaver');
